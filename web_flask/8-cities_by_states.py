f94bf42aa553822d5ae3d597d6f0a3650ab1a1e2
chaher13 <chahercrbs@gmail.com>
2023-08-15 13:27:18 +0200

    Add: a script that starts a Flask web application, display a HTML page: (inside the tag BODY) H1 tag: “States” UL tag: with the list of all State objects present in DBStorage sorted by name (A->Z) tip LI tag: description of one State: <state.id>: <B><state.name></B> + UL tag: with the list of City objects linked to the State sorted by name (A->Z) LI tag: description of one City: <city.id>: <B><city.name></B>

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python3
+
+"""This is a script thats starts a Flask web applications"""
+
+from models import storage
+from models.state import State
+from flask import Flask, render_template
+
+blog_app = Flask(__name__)
+
+
+@blog_app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """
+    Responds with a message 'Hello HBNB!' to HTTP requests
+    to the root URL ("/").
+    """
+    return "Hello HBNB!"
+
+
+@blog_app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """
+    Responds with a message 'HBNB' to HTTP requests to the '/hbnb' URL.
+    """
+    return "HBNB"
+
+
+@blog_app.route("/c/<text>", strict_slashes=False)
+def c_text(text="is cool"):
+    """
+    Responds with a modified text 'C <modified_text>' to requests
+    to the '/c/<text>' route.
+    """
+
+    underscore_by_a_space = text.replace('_', ' ')
+    return "C " + underscore_by_a_space
+
+
+@blog_app.route("/python/", strict_slashes=False)
+@blog_app.route("/python/<text>", strict_slashes=False)
+def python_text(text="is cool"):
+    """
+    Responds with a text 'Python <modified_text>'
+    based on the provided text parameter.
+    """
+    underscore_by_a_space = text.replace("_", " ")
+    return "Python " + underscore_by_a_space
+
+
+@blog_app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    return f"{n} is a number"
+
+
+@blog_app.route("/number_template/<int:n>", strict_slashes=False)
+def number_template_n(n):
+    return render_template("5-number.html", number_t=n)
+
+
+@blog_app.route("/number_odd_or_even/<int:n>")
+def number_odd_or_even(n):
+    return render_template("6-number_odd_or_even.html", number_t=n)
+
+
+@blog_app.route("/states_list", strict_slashes=False)
+def states_list():
+    return render_template("7-states_list.html", states=storage.all(State))
+
+
+@blog_app.route("/cities_by_states", strict_slashes=False)
+def cities_by_states():
+    return render_template("/8-cities_by_states.html", states=storage.all(State))
+
+
+@blog_app.teardown_appcontext
+def teardown(e):
+    storage.close()
+
+
+if __name__ == "__main__":
+    blog_app.run(host="0.0.0.0", port=5000)
